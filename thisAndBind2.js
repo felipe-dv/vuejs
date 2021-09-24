840b38854b7e1d6d05fe1b176ec3b3be8f0c39ba
felipe-dv <felipeifsp2507@gmail.com>
2021-09-24 15:04:33 -0300

    Create thisAndBind2.js

---
+++ thisAndBind2.js
@@ -0,0 +1,11 @@
+function person() {
+    this.age = 0
+
+    const self = this
+    setInterval(function() {
+        self.age++
+        console.log(self.age)
+    }/*.bind(this)*/, 1000)
+}
+
+new person
