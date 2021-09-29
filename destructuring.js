5a08e31af4caf916017d4df736f33694c3ca60e2
felipe-dv <felipeifsp2507@gmail.com>
2021-09-29 15:25:59 -0300

    Create destructuring.js

---
+++ destructuring.js
@@ -0,0 +1,13 @@
+// Destructuring
+
+// String (brackets)
+const [l, e, ...tters] = 'Cod3r'
+console.log(l, e, tters)
+
+// Array (brackets)
+const [x, y] = [1, 2, 3]
+console.log(x, y)
+
+// Attributes of an object (braces)
+const { age: i, name } = { name: 'John', age: 9 }
+console.log(i, name)
