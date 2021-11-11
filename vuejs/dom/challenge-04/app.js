ee3e1cdedd809df65b867bd4dd5bd7d56ff69b6a
felipe-dv <felipeifsp2507@gmail.com>
2021-11-11 19:25:37 -0300

    commit

--- vuejs/dom/challenge-04/app.js
+++ vuejs/dom/challenge-04/app.js
@@ -1,13 +1,15 @@
 new Vue({
-	el: '#desafio',
+	el: '#challenge',
 	data: {
-
+		class1: 'emphasis'
 	},
 	methods: {
-		iniciarEfeito() {
-
+		startEffect() {
+			setInterval(() => {
+				this.class1 = this.class1 == 'emphasis' ? 'shrink' : 'emphasis'
+			}, 1000)
 		},
-		iniciarProgresso() {
+		startProgress() {
 
 		}
 	}
